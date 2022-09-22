945706dde795056c83a139d172239c12e8afc019
Christian Pojoni <dev@pojoni.at>
2022-09-22 21:14:07 +0200

    create dirs + files

--- swift_syntax/pipelines.py
+++ swift_syntax/pipelines.py
@@ -4,40 +4,90 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+import glob
+import os
+import shutil
+from typing import IO
+
+import humps
+import re
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from scrapy import Selector
-from swift_syntax.items import SectionItem
+
 from parser.item_parser import Parser
+from swift_syntax.items import SectionItem
+
+suffix = '.ebnf'
+basedir = 'grammar'
 
 
 class SwiftSyntaxPipeline():
+    def __init__(self):
+        super().__init__()
+        files = glob.glob(f'{basedir}/*')
+        for f in files:
+            try:
+                os.remove(f)
+            except PermissionError:
+                shutil.rmtree(f)
+
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
 
         if adapter.is_item_class(SectionItem):
-            self.parse_section(item)
+            dirname = self.parse_section(item)
+
+            include_file = os.path.join(basedir, '_index.ebnf')
+
+            with open(include_file, 'a') as includes:
+                includes.write('#include :: "{}"\n'.format(os.path.join(dirname, '_index' + suffix)))
+
         return item
 
     def parse_section(self, item):
         title = f'  {item["title"]}  '
-        print('(* ' + len(title) * '-' + ' *)')
-        print('(* ' + title + ' *)')
-        print('(* ' + len(title) * '-' + ' *)')
+        header = '\n'.join([
+            '(* ' + len(title) * '-' + ' *)',
+            '(* ' + title + ' *)',
+            '(* ' + len(title) * '-' + ' *)'
+        ])
+        print(header)
+
+        dirname = humps.kebabize(title.strip().lower())
+        os.makedirs(os.path.join(basedir, dirname))
+
+        files = []
         for group in item['groups']:
-            self.parse_group(group)
+            files.append(self.parse_group(group, dirname))
         print()
 
-    def parse_group(self, item):
+        include_file = os.path.join(basedir, dirname, '_index' + suffix)
+        with open(include_file, 'w') as includes:
+            includes.writelines(map(lambda filename: f'#include :: "{filename}"\n', files))
+        return dirname
+
+    def parse_group(self, item, dirname):
         title = f'{item["title"]}'
+        header = '(* ' + title + ' *)\n'
         print()
-        print('(* ' + title + ' *)')
-        print()
-        for definition in item['defs']:
-            self.parse_def(definition)
+        print(header)
+
+        filename = re.sub(r'Grammar (of)?\s+?(an|a)? ', '', title)
+        filename = humps.kebabize(filename).lower() + suffix
+        assert filename
+        path = os.path.join(basedir, dirname, filename)
+        with open(path, 'w') as file:
+            file.write(header + '\n')
+            for definition in item['defs']:
+                self.parse_def(definition, file)
+
         print()
 
-    def parse_def(self, item: Selector):
+        return filename
+
+    def parse_def(self, item: Selector, file: IO):
         parser = Parser()
         line = parser.parse(item)
+        file.write(str(line) + '\n')
         print(line)
